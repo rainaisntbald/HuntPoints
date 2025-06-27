7d2161f31dd6000a11a0bfe0f7c355f21ff6e92d
rainaisntbald <bumpjammy@gmail.com>
2025-06-27 01:49:27 +0000

    First commit

---
+++ src/main/java/org/iridiummc/huntPoints/HuntPoints.java
@@ -0,0 +1,17 @@
+package org.iridiummc.huntPoints;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HuntPoints extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
